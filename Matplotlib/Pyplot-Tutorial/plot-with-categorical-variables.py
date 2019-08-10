8e4b3281654ea009e6d91c7f345fec50f5d5c8c5
Kiều Đăng Nam <47108512+ChrisZangNam@users.noreply.github.com>
2019-08-10 23:05:23 +0700

    Matplotlib: add Subplot document

--- Matplotlib/Pyplot-Tutorial/plot-with-categorical-variables.py
+++ Matplotlib/Pyplot-Tutorial/plot-with-categorical-variables.py
@@ -1,1 +1,15 @@
 #Ve do thi voi cac bien phan loai
+import matplotlib.pyplot as plt
+import numpy as np
+
+names = ['group_a', 'group_b', 'group_c']
+values = [1, 10, 100]
+
+plt.subplot(131)
+plt.bar(names, values)
+plt.subplot(132)
+plt.scatter(names, values)
+plt.subplot(133)
+plt.plot(names, values)
+plt.suptitle('Categorial Plotting')
+plt.show()
