a17bd57cc5d044652d64914b7f74925e58c87b30
Kiều Đăng Nam <47108512+ChrisZangNam@users.noreply.github.com>
2019-08-21 22:57:45 +0700

    TensorFlow: add example 3 and new document

---
+++ TensorFlow/3_Variable-addition.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+a = tf.Variable(10) #10: gia tri se duoc khoi tao ban dau
+b = tf.Variable(29) #29: gia tri se duoc khoi tao ban dau
+#a, b chua co gia tri den khi no duoc khoi tao trong phien(session)
+
+c = tf.add(a,b)
+#Ham khoi tao gia tri ban dau cho cac bien x,y
+#chu y: Khac voi lap trinh thong thuong, day moi dung lai o viec dinh nghia ham
+#Ham initialize_all_variables() chua thuc su chay, x va y node chua he co gia tri
+init = tf.initialize_all_variables()
+
+#Chay Graph
+with tf.Session() as sess:
+    # tf.initialize_all_variables luc nay chay thuc su -> cac node x, y moi bat dau co gia tri
+    sess.run(init)
+    sess.run(c) #Chay Graph
+    print(c.eval())
